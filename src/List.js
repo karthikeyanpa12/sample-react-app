0df1c18c7792c50313122bc91d93acf05f39f867
karthikeyan <karthikeyan.annamalai@5datainc.com>
2022-03-14 21:01:54 +0530

    fix(error):redirect login 401 error

--- src/List.js
+++ src/List.js
@@ -1,19 +1,41 @@
 import { useState, useEffect } from "react";
 import ListItem from "./ListItem";
+import axios from "axios";
 
 const List = () => {
   const [posts, setPost] = useState([]);
 
+  const fetchPosts = async (url) => {
+    const res = await axios.get(url);
+    setPost(res.data);
+  };
+
   useEffect(() => {
     const url = "https://jsonplaceholder.typicode.com/posts";
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setPost(json));
+
+    //Option 1
+
+    // fetch(url)
+    //   .then((response) => response.json())
+    //   .then((json) => setPost(json));
+
+    //Option 2
+    // axios
+    //   .get(url)
+    //   .then((response) => {
+    //     setPost(response?.data);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+
+    //Option 3
+    fetchPosts(url);
   }, []);
 
   return (
     <ul>
-      {posts.map((post) => (
+      {posts?.map((post) => (
         <ListItem key={post.id} post={post} />
       ))}
     </ul>
